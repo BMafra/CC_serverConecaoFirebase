d1ff700d162fee7444a3b1efc4ac68b09d9287bf
BMafra <bruna_a_mafra@estudante.sesisenai.org.br>
2022-06-20 20:38:32 -0300

    commit

---
+++ api/usuarios/usuarios.controler.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+// arquivo responsavel por rotas
+const usuariosHandler = require("./usuarios.handler");
+
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
