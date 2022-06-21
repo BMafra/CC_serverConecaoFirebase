edc37681e38143f1d0be9cd15c8d6829eefaa26d
BMafra <bruna_a_mafra@estudante.sesisenai.org.br>
2022-06-20 21:13:10 -0300

    commit

--- crud/index.js
+++ crud/index.js
@@ -1,6 +1,7 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue} = require('firebase-admin/firestore');
+const { initializeApp, applicationDefault, cert } = require('firebase/app');
+const { getFirestore, collection, getDocs, setDoc, addDoc } = require('firebase/firestore/lite');
 //conecta bd
+
 const firebaseConfig = {
     apiKey: "AIzaSyDJd11ecSAz79RrDuYsvWraCmDfj_p6hHQ",
     authDomain: "conecao-banco-dados.firebaseapp.com",
@@ -9,28 +10,28 @@ const firebaseConfig = {
     messagingSenderId: "682371083729",
     appId: "1:682371083729:web:ac6f936b01e28e758244f0",
     measurementId: "G-7G60B2PFD9"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-async function save(nomeTabela, id, dado){
-    if(id){
-        const referenceEntity = await db.collection(nomeTabela).doc(id).set(dado);
+async function save(nomeTabela, id, dado) {
+    if (id) {
+        const referenceEntity = await setDoc(doc(db, nomeTabela, id), dado);
         const savedData = {
             ...dado, //savedData = dado / duplicando/ tira referencia de memoria
             id: id
         }
         return savedData
     } else {
-        const referenceEntity = await db.collection(nomeTabela).add(dado);
+        const referenceEntity = await addDoc(collection(db, nomeTabela), dado);
         const savedData = {
             ...dado, //savedData = dado / duplicando
             id: referenceEntity.id
         }
         return savedData;
-    }    
+    }
 }
 
 module.exports = {
